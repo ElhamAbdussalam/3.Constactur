27f19b0cf9fa60900f4fdbbe57943e8a2d21b787
Punisherrr1945 <muhammadelhamcity7@gmail.com>
2025-06-03 10:50:44 +0700

    first commit

--- produk.php
+++ produk.php
@@ -1,6 +1,45 @@
 <?php
 
-class Produk {}
+class Produk
+{
+    public
+        $judul = "judul",
+        $penulis = "penulis",
+        $penerbit = "penerbit",
+        $harga = 0;
 
-$produk1 = new Produk();
-$produk2 = new Produk();
+    // public function sayHello()
+    // {
+    //     return "Hello World!";
+    // }
+
+    public function getLabel()
+    {
+        return "$this->penulis, $this->penerbit";
+    }
+}
+
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
+
+// $produk2 = new Produk();
+// $produk2->judul = "Uncharted";
+// $produk2->tambahProperty = "hahahaha";
+// var_dump($produk2);
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Mashashi Kismitho";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = 3000;
+
+$produk4 = new Produk();
+$produk4->judul = "Uncharted";
+$produk4->penulis = "Neil Druckmann";
+$produk4->penerbit = "Sony Computer";
+$produk4->harga = 250000;
+
+echo $produk3->getLabel();
+echo "<br>";
+echo $produk4->getLabel();
